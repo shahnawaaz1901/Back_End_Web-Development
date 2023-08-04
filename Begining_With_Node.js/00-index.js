c25e54b4fbc9166b60a9a97088b89752af6cf96f
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-04 20:22:03 +0530

    Correct Something

--- Begining_With_Node.js/00-index.js
+++ Begining_With_Node.js/00-index.js
@@ -6,7 +6,7 @@ function add(a, b){
     return a + b;
 }
 
-// Node.Js Works On VAT Engine VAT engine Written in C++ Language
+// Node.Js Works On VAT Engine V8 engine Written in C++ Language
 console.log(add);                                   // Print Function
 console.log(add());                                 // Print NaN Because You Not Pass Any Argument so value of and b is undefined and sum of two undefined is NaN(Not a Number)
 console.log(add(2, 4));                             // Print 6
---
+++ Begining_With_Node.js/03-What_Is_the_Output.js
@@ -0,0 +1,9 @@
+/*
+NodeJS Engine
+Node uses _________ engine in core.
+
+1. SpiderMonkey
+2. Microsoft Chakra
+3. Chrome V8                            // Correct
+4. Node En
+*/
