39b2ef66ead59f4248d1cdd1e9ca30a7bb5b7a8e
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-04 20:07:29 +0530

    Print Function Values

--- Begining_With_Node.js/00-index.js
+++ Begining_With_Node.js/00-index.js
@@ -1,3 +1,12 @@
 /* Since Node.Js is RunTime Environment Over JavaScript So that All Program Saves With .js Extension */
 
 console.log('Hello World !');
+
+function add(a, b){
+    return a + b;
+}
+
+// Node.Js Works On VAT Engine VAT engine Written in C++ Language
+console.log(add);                                   // Print Function
+console.log(add());                                 // Print NaN Because You Not Pass Any Argument so value of and b is undefined and sum of two undefined is NaN(Not a Number)
+console.log(add(2, 4));                             // Print 6
