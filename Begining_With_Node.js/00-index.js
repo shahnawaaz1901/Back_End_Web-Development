b7b14e32d905a6347ca28ce6e8c40340e5ae2ac5
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-04 21:36:46 +0530

    Completed The Beginning With Node.Js

--- Begining_With_Node.js/00-index.js
+++ Begining_With_Node.js/00-index.js
@@ -10,3 +10,8 @@ function add(a, b){
 console.log(add);                                   // Print Function
 console.log(add());                                 // Print NaN Because You Not Pass Any Argument so value of and b is undefined and sum of two undefined is NaN(Not a Number)
 console.log(add(2, 4));                             // Print 6
+
+/* Node Package Manager(npm) is Required to Use Package Library  */
+/* npm Automatically Installed With Node Installation */
+/* You Can CheckOut Whole Library and Packages of Node.Js on https://www.npmjs.com/ */
+/* Lodash is One of the Most Popular Package and Helps us to Deal With Array and Objects In JavaScript */
---
+++ Begining_With_Node.js/05-What_is_the_Output.js
@@ -0,0 +1,34 @@
+/*
+Reusable Javascript File :-
+
+Simple or complex functionality organized in a single or multiple JavaScript files which can be reused 
+throughout your Node.js application is called ________.
+
+1. Module                               // Correct
+2. Library
+3. Package
+4. Function
+*/
+
+/*
+Accessing Node Module Externally :- 
+
+How Node.js modules are made available externally?
+
+1. module.exports                       // Correct Syntax, This syntax is Used For Exports functions
+2. module.expose
+3. module.imports
+4. module.spread
+5. None of the above
+*/
+
+/*
+Including a Module :- 
+
+Which function is used to include modules and libraries in Node Js?
+
+1. include();
+2. require();                           // Correct, In require keyWord You Write Path with in Parenthesis [Like :- require('./04-Modules_Add_Sub.js')]
+3. attach();
+4. None of the above
+*/
