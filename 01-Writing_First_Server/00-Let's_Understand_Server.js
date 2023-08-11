eca3453e1d87ad6475a6d2d0f386b41ea22d75e6
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-11 20:08:41 +0530

    Learn Some Basic Terminology Regarding Server

---
+++ 01-Writing_First_Server/00-Let's_Understand_Server.js
@@ -0,0 +1,47 @@
+/*
+Server : As Name Defines Server Serves Files to the User. 
+*/
+
+/* 
+ISP : ISP Stands for Internet Service Provider. 
+    It refers To the Brand Which Internet is Used by Us for Transfering Information 
+    Over the Internet 
+*/
+
+/* 
+URL : URL Stands For Uniform Resource Locator
+*/
+
+/*
+TCP : Transmission Control Protocol
+*/
+
+/*
+IP : IP stands For Internet Protocol
+*/
+
+/*
+WhenEver Data is Transfered Over the Internet Some Set of Instruction is Used to Enstalish the 
+Connection to the Server or Server to the Browser, this Set of Instruction is Called Protocol
+*/
+
+/*
+Protocol Helps to Connect Two Independent Machines.
+*/
+
+/* 
+When You Open A Browser And Enter URL google.com then Browser Sends Request to google.com Server 
+With the Help of ISP and WhatEver Code and File is Present For google.com returns or respond By The Server
+to our Browser and Browser Render that Code and Files. This is The Very Basic Process.
+*/
+
+/*
+If You go to Facebook.com and Login the Account then Page of Facebook.com is Same that in left Side
+there is Groups Section and Right side there's Chat Section and in Center Timeline Section.
+This Whole Sections is Common For All User Only Data is Changed For Every User. Because Every User
+Have Have Different Friends Different Groups So that Data is Specific to Every User But Interface
+is Same to All User.
+When you Login then Browser Sends Request that this User is Login Give Me the Data of Friends
+Groups and Gives the data WhatEver Pages and peoples this User Follow.
+So that User Can See Only that Data Which is Required for That User and Like By That User
+*/
