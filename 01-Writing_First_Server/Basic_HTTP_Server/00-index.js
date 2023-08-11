a345949c7210473ab45842c3321bc842008acdef
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-11 21:37:31 +0530

    Giving Response to the Server

--- 01-Writing_First_Server/Basic_HTTP_Server/00-index.js
+++ 01-Writing_First_Server/Basic_HTTP_Server/00-index.js
@@ -13,6 +13,25 @@ file and files Return Some Logic Which is Written in that Server
 */
 
 const http = require('node:http');           // For Accessing http Module First Import That Module into this file
+
+// Function takes Two Arguments one is Required and One is Response
+function responseServer(required, response){
+    // required argument gives us required thing which User Want
+    /*
+    For Example if Our Server is hotel.com and User Want Contact Section then URL of that section is 
+    hotel.com/contact so this gives us '/contact' that gives us idea that user want contact Section So You 
+    Can Add Multiple if else Condition that if User Click on contact which page user get if User Click over currentBook
+    section then what page User gets
+    */
+    console.log(required.url);          // Print Require URLs
+    response.end('Gotcha');             // Argument Which is Passed as String Shows at User End
+}
+
+
+
+
+
+
 /*
 WhenEver Your System Sends Request it Sends Request directly to Port 8000 which is Default
 A single Port Work a Single Work. It's Not Like USB Ports which our System Own.
@@ -23,9 +42,10 @@ One Port Can Run only One Software at One Time
 */
 const port = 8000;                      // Port is a Perticular Area of that Server it Means in Which Perticular hardware Server is Loaded, 8000 is a Unique Key Number of that Perticular Hardware
 // Create Server
-const server = http.createServer();
+// For Handle Response You Need to Pass a function as argument in createServer Function to handle Response
+const server = http.createServer(responseServer);
 
-// Server Listen is Called via Port and an Function which is Responsible for Any Kind of \
+// Server Listen is Called via Port and an Function which is Responsible for Any Kind of
 // Error Occurs and Tells Us Server Work Fine or Not
 server.listen(port, function(error){
     if(error){
