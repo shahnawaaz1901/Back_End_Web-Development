2b0fe5e0956bea944a97a08b3704e651f5932792
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-12 13:27:28 +0530

    Use HTML File to Respond

--- 01-Writing_First_Server/Basic_HTTP_Server/00-index.js
+++ 01-Writing_First_Server/Basic_HTTP_Server/00-index.js
@@ -14,8 +14,10 @@ file and files Return Some Logic Which is Written in that Server
 
 const http = require('node:http');           // For Accessing http Module First Import That Module into this file
 
+const fs = require('fs');                   // InBuilt Module Which Helps to Read HTML Files and res
+
 // Function takes Two Arguments one is Required and One is Response
-function responseServer(required, response){
+function responseServer(required, response) {
     // required argument gives us required thing which User Want
     /*
     For Example if Our Server is hotel.com and User Want Contact Section then URL of that section is 
@@ -24,7 +26,27 @@ function responseServer(required, response){
     section then what page User gets
     */
     console.log(required.url);          // Print Require URLs
-    response.end('Gotcha');             // Argument Which is Passed as String Shows at User End
+    // It Always Print Favicon.io Because of Favicon Image of the Page
+
+    /* For Reading HTML File You Need Something Which Read that HTML File and Send that HTML File When Browser Request */
+    //In Node.Js You can't Write Directly File Name Always Write in Directory Form
+    fs.readFile('./index.html', function(error, data) {      // This is Asynchronous Function
+        if(error){
+            console.log('Error', error);
+            return response.end('<h1> Error !</h1>');
+        }
+
+        return response.end(data);
+    });                    
+
+
+    /*For Giving HTML file as Response or Giving Some HTML or Text Content You Need to write this
+     First Argument is Response Code, Response Code vary According to Request but we just give 200 
+     as default for all request just for now*/
+    
+    // response.writeHead(200, {'content-type':'text/html'});
+    // We Can Also Give Styles
+    // response.end();             // Argument Which is Passed as String Shows at User End
 }
 
 
@@ -47,8 +69,8 @@ const server = http.createServer(responseServer);
 
 // Server Listen is Called via Port and an Function which is Responsible for Any Kind of
 // Error Occurs and Tells Us Server Work Fine or Not
-server.listen(port, function(error){
-    if(error){
+server.listen(port, function (error) {
+    if (error) {
         console.log(error);
         return;
     }
@@ -59,5 +81,5 @@ server.listen(port, function(error){
 // In a Computer localhost is server which is similar to 127.0.0.1 which is an Ip and Hard to remember
 // Along with localhost and Ip write port Number after colon
 
-// If We Run Our Server Right Now then it's Load Continuesly Because we Did'nt return anything 
+// If We Run Our Server Right Now then it's Load Continuesly Because we Did'nt return anything
 // after server is Run Successfully
