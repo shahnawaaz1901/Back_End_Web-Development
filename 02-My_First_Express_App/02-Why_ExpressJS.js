13deb76b72db35aaa6e803db4e612ec6ac8d6404
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-15 21:14:07 +0530

    Understand Pros and Cons of Express.Js

---
+++ 02-My_First_Express_App/02-Why_ExpressJS.js
@@ -0,0 +1,15 @@
+/* Express.Js is Most Popular FrameWork of Node.Js */
+
+/* Express.Js is Unopiniated(Pros and Cons) Framework */
+
+// Cons
+/* 
+Express Does't Tell you To Structure Your Folder or Code in Specific Manner.
+*/
+/* 
+So that as Beginner We Could stray into makein wrong folder structuring choices or 
+not so Good.
+*/
+
+//Pro
+/* Express.Js Allows You to Scale Up Your Application Easily */
