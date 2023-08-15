2795325968657c00e7825078dc8a5492bee68b07
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-15 21:24:30 +0530

    Some Basic Around Express.JS

---
+++ 02-My_First_Express_App/03-What_is_Output.js
@@ -0,0 +1,51 @@
+/*
+Express
+
+Is Express the only framework for Node.js?
+
+1. Yes
+2. No               // Correct, Express.js is Most Popular FrameWork Other Node.Js Based FrameWork Exist
+*/
+
+/*
+MVC Framework
+
+What is the extended name for MVC?
+
+1. Model Visual Control
+2. Model Visual Controller
+3. Model View Controller                // Correct
+4. None of the above
+
+*/
+
+/*
+package.json
+
+package.json contains all the dependencies required by express.
+
+1. True
+2. False                            // Correct, Because package-lock.JSON contains all Dependencies Required By Express.Js, package.JSON contains Dependency Required By Node.JS
+*/
+
+/*
+Core Features Express
+
+What are core features of Express framework?
+
+1. Allows to set up middlewares to respond to HTTP Requests
+2. Defines a routing table which can work as per HTTP Method and URL
+3. Dynamically render HTML Pages
+4. All of the above                                             // Correct
+*/
+
+/*
+node_modules
+
+What does the 'node_modules' folder contain?
+
+1. Libraries that Node.js requires                      // Correct
+2. Contains the application structure
+3. Libraries that Express requires
+4. None of the above
+*/
