6a7d47cac0e57eb7408402827968bf13aa9f1c4d
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-15 21:06:08 +0530

    UnderStand MVC Architechture

---
+++ 02-My_First_Express_App/01-UnderStand_MVC_Model_View_Controller.js
@@ -0,0 +1,27 @@
+/* Most Important Thing For Select A Web FrameWork is Architecture */
+
+/* Multiple Architecture Exist to Select a Web Framework */
+
+/* But We Use MVC Architecture for No Reason */
+
+/* MVC Means Model, Controller and View */
+
+/* UnderStand */
+
+/* 
+WhenEver Your Browser Sends Request For You To Render WebPage First it Goes to Router,
+Router checks the Request type Like it's a Homepage Request For a Perticular Web Page or
+Contact Request or Some Other kind of Requests. Then Request Type is Gone to 'Controller' then
+finds the mapped function corresponding to that Request and After that Controller Goes to 'View'
+View Helps Controller to Read the Relevent file and After Reading 'View' sends Data to the Controller
+Controller Sends Data to the Browser and Browser Render that Web Page
+Sometime Controller Require Some Specific Content to a Specific User, 
+Like in facebook For a Specific Person Friends are Different From Others. For Getting 
+Information Controller Sends Request to 'Model', 'Model' is Directly Interconnected to Database
+(Database is Permanent Storage Where All Data of All Users is Stored). and get Relevent 
+Information From DataBase and Send Information to Controller, Controller Sends Information to
+'View' to Read the File and After that sends Data to Browser and Browser Render All Information
+to the Screen.
+*/
+
+/* Here Controller, View and Model is set of Functiosns */
