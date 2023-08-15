f055f16aba0dc3c797e774276ef1ce6b2697c139
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-15 20:03:23 +0530

    UnderStand Web

---
+++ 02-My_First_Express_App/00-Web_FrameWork.js
@@ -0,0 +1,13 @@
+/* What is Web FrameWork and Why we Use Web FrameWork */
+
+/* 
+For Maintaining a Heavy File Which is Difficult to Manage So file is devide into subFiles and 
+Every File if Heavy and Hard to manage then again every sub file devide into another sub file.
+and if a perticular Feature is Required in all SubFiles then it's Hard to Link that Feature in Every
+Sub File So that, Solving this Problem Web FrameWork is Introduce.
+*/
+
+/*
+Web FrameWork Makes our Life Easier and Write Less Code and Do More Work, So We Use Web FrameWork
+Express.Js is a Web Framework Basis on Node.Js
+*/
