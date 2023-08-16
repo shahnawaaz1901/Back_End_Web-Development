4a171e65806e307124d04b0d629eb0857c77b349
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-16 20:27:15 +0530

    Some Basic Questions on Express.js

---
+++ 02-My_First_Express_App/04-What_is_the_Output.js
@@ -0,0 +1,49 @@
+/*
+Install Express
+
+Which command will install express in your project?
+
+1. node install express
+2. node install -g express
+3. node install express save
+4. npm install express                          // Correct
+ 
+*/
+
+/*
+Express Server
+
+Which among these will run our express server on port 3001?
+
+i.    const express = require('express');
+      const port = 3001;
+      const app = express();
+      app.listen(port, function(err){
+      console.log('Yup!My Server is running on Port', port);
+      })
+
+ii.   const http = require('http');
+      const port = 3001;
+      const server = http.createServer();
+      server.listen(port, function(err){
+      console.log('Yup!My Server is running on Port', port);
+      });
+
+iii.  const express = require('express');
+      const port = 3001;
+      const app = express();
+      app.start(port, function(err){
+      console.log('Yup!My Server is running on Port', port);
+      })
+
+iv.   const express = require('express');
+      const port = 3001;
+      app.listen(port, function(err){
+      console.log('Yup!My Server is running on Port', port);
+      })
+
+1. i                                    // Correct
+2. ii
+3. iii
+4. iv
+*/
