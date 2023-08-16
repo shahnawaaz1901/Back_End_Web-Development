93bd25cb4ef14a8eca4d9a74f9533c75e74a760d
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-16 21:30:37 +0530

    Learn Basics Around Template Engines and Learn Usage of Template Engine

---
+++ 02-My_First_Express_App/05-Template_Engine.js
@@ -0,0 +1,26 @@
+/* Template is What We Know Already in C++ */
+
+// Template allows us specify dataType at the time of declaration for a specific data type
+
+/* 
+For Example if You make a class whithout template then You wouldn't be able to Use that 
+Class for Another Data Type. For Use in Multiple Data Type of Single Class You Can Use
+Template, Similar Type of Work is Done by Template Engine in BackEnd Web Development
+*/
+
+/*
+For Example :- 
+In FaceBook Front End Code is Same , multiple Sections is Same for All User, But Content
+is Different for All User, it's Not Mean that Developer Write Code For Every User.
+Developer Create that Front End Web Page Using Template Engine So that When Ever a User 
+Specific Data(Eg. Friends, chats, Timeline) is Required it goes to Database fetch Data or 
+Link from database for that Specific User and Display Things Over the Browser
+*/
+
+// Multiple Template Engines Are Exist :-
+/*
+    Some Most Popular Ones
+1. EJS :- Embeded JavaScript
+2. HBS :- HandleBar.Js
+3. Pug.Js 
+*/
