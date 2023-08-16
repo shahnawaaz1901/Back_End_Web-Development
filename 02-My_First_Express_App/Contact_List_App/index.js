8f0111ecfac0cf832263c7695e6e14a9a1ff6591
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-16 20:09:35 +0530

    Response to the Server Using Express.Js

--- 02-My_First_Express_App/Contact_List_App/index.js
+++ 02-My_First_Express_App/Contact_List_App/index.js
@@ -8,6 +8,36 @@ const port = 8000;
 // all functionalities of Express now store in our app Variable
 const app = express();
 
+/* 
+Instead of Check URL in switch cases and after that find corresponding function for that 
+switch case You Can Directly Write get and pass it two argument first is request URL
+and second is corresponding that URL a callback function
+
+*/
+// This Callback also get two Argument First is request and second is response
+app.get('/',(request, response)=>{
+    console.log('Request URL', request.url);
+    // response.send('Hello Baby');
+    /* 
+    Using Express.Js it is Easy to Send Response to the Request Because we don't Need to 
+    write writeHead function to specify text/html content express will automatically Detect
+    What Response we want to send. 
+    */
+
+    /* 
+    For Returning an HTML Element You Can Directly Return withOut telling HTML content
+    to the browser express will do this for you by detecting automatically
+    */
+   response.send('<h1>Hello Baby</h1>');
+   /*
+   this Statement using express.Js Tells Automatically to Browser hey this is an HTML 
+   element and browser will Render it according to that
+   */
+});
+
+
+
+
 // listen function takes two Argument one is Port and Another is Callback
 // listen callback takes error as first Argument
 app.listen(port, (error)=>{
