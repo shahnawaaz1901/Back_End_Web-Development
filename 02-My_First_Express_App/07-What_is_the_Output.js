3f4aef78516312aee6751b9c4eaffc597fc223aa
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-17 20:51:33 +0530

    Some Basic Questions Around EJS JavaScript Functionalities

---
+++ 02-My_First_Express_App/07-What_is_the_Output.js
@@ -0,0 +1,64 @@
+/*
+For Loop
+
+Which among these will print numbers from 1 to 5?
+
+i.    <% for (let i=1; i<=5; i++) { %>
+        <%= i %>          
+      <%}%>
+
+ii.   <% for (let i=1; i<10; i++) { %>
+        <% i %>          
+      <%}%>
+
+iii.  <%= for (let i=1; i<10; i++) { %>
+        <%= i %>          
+      <%}%>
+
+iv.   <% for (let i=1; i<10; i++) { %>
+        <%= i %>          
+      <%=}%>
+
+1. i                        // Correct
+2. ii
+3. iii
+4. iv
+
+*/
+
+/*
+If Else
+
+Which among these is a valid if-else statement?
+
+i.   <% if true{ %>
+       <h1>It's true!</h1>          
+     <% }else{ %>
+       <p>It's false :(</p>
+     <% } %>
+
+ii.   <% if (true){ %>
+        <h1>It's true!</h1>          
+      <% else %>
+        <p>It's false :(</p>
+      <% } %>
+
+
+iii.  <% if (true){ %>
+        <h1>It's true!</h1>          
+      <% }else{ %>
+        <p>It's false :(</p>
+      <% } %>
+
+
+iv.  <%= if (true){ %>
+       <h1>It's true!</h1>          
+     <% }else{ %>
+       <p>It's false :(</p> 
+     <% } %>
+
+1. i
+2. ii
+3. iii                                  // Correct, Syntax is Correct
+4. iv
+*/
