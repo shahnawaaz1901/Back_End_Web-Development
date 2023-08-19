1b4f2920f6e956917bdbb042b059a9c74e132e3f
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-19 19:32:33 +0530

    Learn to Access Post Request Form Data

--- 03-My_First_Express_App/contact/index.js
+++ 03-My_First_Express_App/contact/index.js
@@ -4,6 +4,7 @@ const express = require('express');             // import express
 const port = 8000;
 const app = express();                          // Use App With express Function
 app.set('view engine', 'ejs');                  // Set view engine to ejs
+// First Argument is views means this views then second time in path there is folder views so both are diiferent
 app.set('views', path.join(__dirname, 'views')) // Set View Engine Path
 
 var contactList = [
@@ -25,6 +26,21 @@ var contactList = [
 app.get('/',function(request, response){        // First Argument of get is Required url
     return response.render('contact',{contact : contactList});
 })
+
+app.get('/practice',function(request, response){
+    return response.render('practice');
+})
+
+// Post Request, Because we Create Contact and Try to Save it
+// Always when You Create a Handle Function for Post request.url is same in action(in HTML form tag) as mention in function
+app.post('/create-contact',function(request, response){
+    console.log(request);
+    // a function get is required for handle this practice URL to render it On Browser at line 30
+    // response.redirect('/practice');
+    // return;
+})
+
+// WhatEver method we use get or post or any other kind express detect that method and value of that method is true if You Print the Request
 
 app.listen(port,(error)=>{
     if(error){
