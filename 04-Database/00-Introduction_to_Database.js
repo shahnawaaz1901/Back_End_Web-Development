7862b1e4a4cc5a5630f707b4a5befb7695ac36f0
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-22 19:48:58 +0530

    UnderStand Databases and It's Types

---
+++ 04-Database/00-Introduction_to_Database.js
@@ -0,0 +1,33 @@
+/*
+Why Database : When We Restart the Server All of Data Which we Received and Store 
+            is Gone or Destroyed Because Data is Store in Our Ram And Ram is Temporary Storage.
+            For Remaining all of Files or Data We Need to Store it SomeWhere Permanently So if
+            We Restart the Server My data is Secure. For Solving This Problem Database is Comes
+            into This Picture
+*/
+
+/*
+Database : Database is Permanent Storage Where We Can Store our Data or Files Permanently
+        and whenever We Want to Access we Can Access with Just Simple Clicks.
+        All Things, Files, Folders of Server is Store in Database. Database No Relation with
+        Server Restart or Other Kind of Server Problem Data is Stored Permanently Until Data is
+        Deleted or Remove By the User
+*/
+
+/*
+DataBase Divide into Two Category :-
+1. SQL(Sequal DataBase) :- MySQL, Postgres (OpenSource)
+2. NOSQL(No Sequal DataBase) :- FireBase(Online Service By Google), Redis, MongoDB (Open Source)
+*/
+
+/*
+Why Two Categories ..?
+    Sequal Database :- In Sequal DataBase Data is Stored in Format of Tables. It's 
+                    Like an Excel Sheet Tableor Other Normal Kind of Table We Used 
+                    in Our Daily Life Using Rows and Columns
+
+    NoSequal Database :- In NoSequal Database Data is Stored in Other then Table Format. 
+                    Most Popular Way of Storing Data in Database is JSON Object (JavaScript
+                    Object Notation). Where Data is Stored in Keys and its Corresponding 
+                    values
+*/
