8f65edade2cd09cf55454083d003f3719b379b30
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-22 19:52:46 +0530

    UnderStand NoSQL and SQL Differences

---
+++ 04-Database/01-What_is_the_Output.js
@@ -0,0 +1,21 @@
+/*
+NoSQL Database
+
+Which of the following is not a NoSQL database ?
+
+1. MongoDB
+2. SqLite 3                                     // Correct
+3. Firebase
+4. None of the Above
+*/
+
+/*
+MongoDB
+
+Under which category of NoSql do mongoDB falls?
+
+1. Wide Column Stores
+2. Document databases                           // Correct
+3. Graph databases
+4. None of the Above
+*/
