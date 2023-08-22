5a164b77c2747c80a73eeeeac2d5c29fdc0f55bc
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-22 21:21:22 +0530

    Benefits of MongoDB

---
+++ 04-Database/02-Why_Mongo_DB.js
@@ -0,0 +1,5 @@
+// Mongo DB is New technology
+// MongoDB is Fast 
+// MongoDB is Easy to UnderStand Because you Just Need to UnderStand Object KeyPair Values
+// Setting Up or Storing Data is Easy Compare to Sequal DataBase where You Creates Table and Put Data in Table
+// MongoDB is Based on JSON which we familier Already
