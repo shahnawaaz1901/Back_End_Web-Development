c2e8bdb7f2950db6c56783ea87af554e394e11f4
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-23 19:39:48 +0530

    Intall MongoDB on System

---
+++ 04-Database/Contact List App/index.js
@@ -0,0 +1,57 @@
+// Including Modules or Require Libraries
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+// Set Port
+const port = 8000;
+
+// Start the Server
+const app = express();
+
+// For Decode Encoded Form Data
+// Use Function is Used to Set MiddleWare
+app.use(express.urlencoded());
+
+
+// Set Path of Other Require Files Using MiddleWare
+// This is Address of Files other then ejs like css, javaScript
+app.use(express.static('asset'));
+
+// Set EJS for View Engine
+app.set('view engine','ejs');
+
+// Set Path Where our ejs File is Stored
+app.set('views',path.join(__dirname,'views'));
+
+let contacts = [];
+
+// Server Request URL
+app.get('/',function(request, response){
+    return response.render('index',{
+        title :"Contact List App",
+        Contact : contacts,
+    });
+})
+// Send Data to the Server and Render It On Browser
+app.post('/create-contact',function(request, response){
+    contacts.push(request.body);
+    response.redirect('/');
+})
+
+app.get('/delete-contact',function(request, response){
+    let phone = request.query.phone;
+    let index = contacts.findIndex((contact)=>contact.phone == phone);
+    contacts.splice(index, 1);
+    response.redirect('/');
+})
+
+// Server Status
+app.listen(port, function(error){
+    if(error){
+        console.log('Error', error);
+        return;
+    }
+    console.log('Server is up And Run SuccessFully');
+})
