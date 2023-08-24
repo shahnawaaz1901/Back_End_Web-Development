4f9e906f18645d0e563017b8f539bc15f2976482
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-24 21:09:50 +0530

    Questions Around Database

---
+++ 04-Database/04-What_is_the_Output.js
@@ -0,0 +1,38 @@
+/*
+Connecting to Databse
+
+Which among these statements will make a connection to the database ‘contactlistdb’ 
+running on localhost when we’re using mongoose?
+
+1. mongoose.connect('contact_list_db');
+2. mongoose.connect('mongodb://localhost/contact_list_db');             // Correct
+3. mongoose.connection('mongodb://localhost/contact_list_db');
+4. mongoose.connect('localhost/contact_list_db');
+*/
+
+/*
+Schema Types in MongoDB
+
+Select all the valid schema types in MongoDB.(Multiple)
+1. String                               // Correct
+2. Number                               // Correct
+3. Date                                 // Correct
+4. Integer
+5. Boolean                              // Correct
+6. Mixed                                // Correct
+7. Float
+8. ObjectId                             // Correct
+9. Array                                // Correct
+*/
+
+/*
+Property of Document
+
+_____is a property set on each document when first created by Mongoose.
+
+1. id
+2. type
+3. versionkey                           // Correct
+4. None of the Above
+
+*/
