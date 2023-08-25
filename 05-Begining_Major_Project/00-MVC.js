43401b1d05f86713b63b7099ef9c81f0998ad942
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-25 19:42:32 +0530

    Starting to Setup Major Project

---
+++ 05-Begining_Major_Project/00-MVC.js
@@ -0,0 +1,11 @@
+/* There Are Few Steps which we Follow to Scale Up Our Web Application */
+/*
+    1. SetUp Directory Structure
+    2. SetUp Express to FireUp the Server
+    3. Use Git to maintain or check Where Changes is Done
+    4. For Scale Up We Need to Set Routers and Controllers in Different Files to Keep Clean Code
+    5. SetUp View Engine to EJS for View
+    6. Partial Layouts
+    7. Static Files, Use for CSS and JavaScript in EJS File
+    8. MongoDB, To Store Data Permanently on Server
+*/
