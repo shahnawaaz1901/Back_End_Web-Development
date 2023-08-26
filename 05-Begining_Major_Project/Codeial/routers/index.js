4205124580ed6e3d5332dcc118d465de5ca0edec
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-26 19:04:55 +0530

    SetUp Controllers & Routes

--- 05-Begining_Major_Project/Codeial/routers/index.js
+++ 05-Begining_Major_Project/Codeial/routers/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+// Import Home Controller
+const homeController = require('../controllers/home');
 // For Managing Routes we Need to Use express's Route function
 const router = express.Router();
+// Set Home Controller For Default Route
+router.get('/',homeController.home)
 console.log('Inside the Route File');               // Verify the Route File
 // Export Router so that our all file use router function
 module.exports = router;
