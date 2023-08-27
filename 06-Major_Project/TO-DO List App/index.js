b05bb6266bda85970b76490e7e27dd58953b470c
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-08-27 19:22:01 +0530

    Try to Config with Database

--- 06-Major_Project/TO-DO List App/index.js
+++ 06-Major_Project/TO-DO List App/index.js
@@ -2,10 +2,14 @@
 const express = require('express');
 const port = 8000;
 const app = express();
+const db = require('./config/index');
 
 // Set View Engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
+// For Decode the Data Received By the User
+app.use(express.urlencoded());
 
 // SetUp Controller For Home Page
 app.get('/',function(request, response){
