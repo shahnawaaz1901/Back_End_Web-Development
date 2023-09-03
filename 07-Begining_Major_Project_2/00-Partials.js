47bafb389e141da0cf3498d833c0cef6ef22708c
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-03 19:10:13 +0530

    UnderStand Partials

---
+++ 07-Begining_Major_Project_2/00-Partials.js
@@ -0,0 +1,7 @@
+/*
+    Partials : As Name Specify, When Ever We Create a Big Project or Some Common 
+            piece of code in project require in every file Like header footer commonly
+            so that instead of writing same code again and again we put that piece of 
+            code in seprarate file so that whenever we require that piece of code we 
+            write only one line and we can use that code in every file
+*/
