817f460adc656aa75d457cfbf9c55c226e443d95
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-03 19:23:10 +0530

    UnderStand Layout

---
+++ 07-Begining_Major_Project_2/01-Layouts.js
@@ -0,0 +1,8 @@
+/*
+    Layouts : Like a Social Media Website Header and Footer are fixed but user information
+            is change according to the user. In Social Media website a timeline part is 
+            change according to the user but size of timeline box is common to all user.
+            Feed or timeline part is Called Variable part Because it Changes According to
+            the User, the whole structure of common and variable part is called Layout.
+
+*/
