88f7e3ba4cba39b5ed5a8f603a5b870db98da29d
Shahnawaaz Ansari <shaan.ansari1901@gmail.com>
2023-09-07 21:42:15 +0530

    Adding Router for Sign in & Sign Up Page

--- 05-Begining_Major_Project/Codeial/controllers/users_controller.js
+++ 05-Begining_Major_Project/Codeial/controllers/users_controller.js
@@ -1,5 +1,21 @@
+// Render User Profile Page
 module.exports.profile = function(request, response){
-    return response.render('users',{
+    return response.render('users_profile',{
         title : "Profile | Codeial",
+    });
+}
+
+
+// Render User SignUp Page
+module.exports.signUp = function(request, response){
+    return response.render('user_sign_up',{
+        title : "Sign in | Codeial",
+    });
+}
+
+// Render User SignIn Page
+module.exports.signIn = function(request, response){
+    return response.render('user_sign_in',{
+        title : "Sign In | Codeial",
     });
 }
--- 05-Begining_Major_Project/Codeial/routers/users.js
+++ 05-Begining_Major_Project/Codeial/routers/users.js
@@ -7,5 +7,13 @@ const postController = require('../controllers/post_controller');
 // Send Request to Controller
 router.get('/profile',userController.profile);
 
+// Send Request to Controller for Sign Up Form
+router.use('/sign-up',userController.signUp);
+
+
+// Send Request to Controller for Sign In Form
+router.use('/sign-in',userController.signIn);
+
+// Send Post Request to the Controller
 router.post('/post',postController.post);
 module.exports = router;
